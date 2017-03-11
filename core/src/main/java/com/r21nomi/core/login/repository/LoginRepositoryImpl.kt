a99385ca99d712577791644e710f6b2c29451fb9
r21nomi <ryotakosu@gmail.com>
2017-03-11 17:49:57 +0900

    Refactor

--- core/src/main/java/com/r21nomi/core/login/repository/LoginRepositoryImpl.kt
+++ core/src/main/java/com/r21nomi/core/login/repository/LoginRepositoryImpl.kt
@@ -1,6 +1,6 @@
 package com.r21nomi.core.login.repository
 
-import android.app.Application
+import android.content.Context
 import android.content.Intent
 import android.net.Uri
 import com.r21nomi.core.R
@@ -14,18 +14,18 @@ import javax.inject.Singleton
  * Created by Ryota Niinomi on 2016/09/26.
  */
 @Singleton
-class LoginRepositoryImpl @Inject constructor(val application: Application,
+class LoginRepositoryImpl @Inject constructor(val context: Context,
                                               val accessTokenPref: AccessTokenPref,
                                               val accessTokenDao: AccessTokenDao) : LoginRepository {
 
     override fun getOAuthIntent(): Intent {
         val param: MutableList<Pair<String, String>> = ArrayList()
-        param.add(Pair("client_id", application.getString(R.string.client_id)))
+        param.add(Pair("client_id", context.getString(R.string.client_id)))
         param.add(Pair("scope", "read_public,write_public"))
         param.add(Pair("response_type", "token"))
-        param.add(Pair("redirect_uri", "pdk" + application.getString(R.string.client_id) + "://"))
+        param.add(Pair("redirect_uri", "pdk" + context.getString(R.string.client_id) + "://"))
 
-        val url: String = ApiUtil.getUrlWithQueryParams(application.getString(R.string.oauth_url), param)
+        val url: String = ApiUtil.getUrlWithQueryParams(context.getString(R.string.oauth_url), param)
         return Intent(Intent.ACTION_VIEW, Uri.parse(url))
     }
 
