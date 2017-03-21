26a8e1dcc2a355f33cedc1ff7f39364837ec31e7
r21nomi <ryotakosu@gmail.com>
2017-03-21 12:59:29 +0900

    Delete some files

--- core/src/main/java/com/r21nomi/core/login/repository/LoginRepositoryImpl.kt
+++ core/src/main/java/com/r21nomi/core/login/repository/LoginRepositoryImpl.kt
@@ -3,6 +3,7 @@ package com.r21nomi.core.login.repository
 import android.content.Context
 import android.content.Intent
 import android.net.Uri
+import com.r21nomi.core.BuildConfig
 import com.r21nomi.core.R
 import com.r21nomi.core.login.usecase.LoginRepository
 import com.r21nomi.core.util.ApiUtil
@@ -21,10 +22,11 @@ class LoginRepositoryImpl @Inject constructor(val context: Context,
 
     override fun getOAuthIntent(): Intent {
         val param: MutableList<Pair<String, String>> = ArrayList()
-        param.add(Pair("client_id", context.getString(R.string.client_id)))
+        val pinterest_client_id = BuildConfig.PINTEREST_API_CLIENT_ID
+        param.add(Pair("client_id", pinterest_client_id))
         param.add(Pair("scope", "read_public,write_public"))
         param.add(Pair("response_type", "token"))
-        param.add(Pair("redirect_uri", "pdk" + context.getString(R.string.client_id) + "://"))
+        param.add(Pair("redirect_uri", "pdk" + pinterest_client_id + "://"))
 
         val url: String = ApiUtil.getUrlWithQueryParams(context.getString(R.string.oauth_url), param)
         return Intent(Intent.ACTION_VIEW, Uri.parse(url))
